ee63a2c30e8f306c86b2506ae196eadaed4e4d58
DevCamp <DevCamp@708S-1023B-12.canyonsdistrict.pvt>
2018-07-26 09:23:02 -0600

    Mapped course data into library course component instances

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -9,14 +9,14 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
                 <div className="library-course__title-check">
-                    <label className="library-course__title">Problem Solving</label>
+                    <label className="library-course__title">{ this.props.title }</label>
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <Arrow className="library-course__arrow"/>
                 <Action className="library-course__action"/>
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia ut lorem vel posuere. Aenean viverra faucibus fringilla. Aliquam fermentum lectus nec porttitor convallis. Cras sollicitudin lorem luctus nunc convallis, non congue quam vestibulum.</p>
+                    <p>{ this.props.description }</p>
                 </div>
             </div>
         )
